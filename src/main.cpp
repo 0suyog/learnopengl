2c3aa532c4b1a762ea75c54bca576f7e0ccb6b77
0suyog <dahalsunischit@gmail.com>
2026-02-27 18:13:52 +0545

    rectangle rendering with two triangles

--- src/main.cpp
+++ src/main.cpp
@@ -51,6 +51,8 @@ int main() {
     return -1;
   }
 
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
   unsigned int vertexshader;
   vertexshader = glCreateShader(GL_VERTEX_SHADER);
   glShaderSource(vertexshader, 1, &vertexShaderSource, NULL);
@@ -94,25 +96,27 @@ int main() {
   glDeleteShader(fragmentshader);
   glDeleteShader(vertexshader);
 
-  // glViewport(0, 0, 800, 600);
+  // float vertices[] = {-0.5, -0.5, 0.0, 0.5, -0.5, 0.0, 0.0, 0.5, 0.0};
+  float vertices[] = {0.5f,  0.5f,  0.0f, 0.5f,  -0.5f, 0.0f,
+                      -0.5f, -0.5f, 0.0f, -0.5f, 0.5f,  0.0f};
 
-  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
-  float vertices[] = {-0.5, -0.5, 0.0, 0.5, -0.5, 0.0, 0.0, 0.5, 0.0};
-  // float vertices[] = {0.5f,  0.5f,  0.0f, 0.5f,  -0.5f, 0.0f,
-  //                     -0.5f, -0.5f, 0.0f, -0.5f, 0.5f,  0.0f};
-  //
-  // unsigned int indices[] = {0, 1, 3, 1, 2, 3};
+  unsigned int indices[] = {0, 1, 3, 1, 2, 3};
 
   unsigned int VAO;
   unsigned int VBO;
+  unsigned int EBO;
 
   glGenVertexArrays(1, &VAO);
   glGenBuffers(1, &VBO);
+  glGenBuffers(1, &EBO);
 
   glBindVertexArray(VAO);
   glBindBuffer(GL_ARRAY_BUFFER, VBO);
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
+               GL_STATIC_DRAW);
 
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
   glEnableVertexAttribArray(0);
@@ -123,13 +127,16 @@ int main() {
 
   while (!glfwWindowShouldClose(window)) {
     process_input(window);
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
 
     glUseProgram(shaderProgram);
     glBindVertexArray(VAO);
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
-    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
     glfwSwapBuffers(window);
     glfwPollEvents();
   }
